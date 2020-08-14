85185f8036c98ed6d40239371cf734ccbfc73606
Greg Stephen <GregAStephen@gmail.com>
2020-08-14 09:47:35 -0500

    quantifiers and compile

--- regex.py
+++ regex.py
@@ -26,3 +26,35 @@ for match in re.finditer(pattern, newtext):
   print(match)
 
 #returns each match object
+
+# \d     a digit
+# \w     alphanumeric
+# \s     white space
+# \D     a non digit
+# \W     non-alphanumeric
+# \S     non-whitespace
+
+
+phonenumbertext = 'My phone number is 408-555-1234'
+
+phone = re.search(r'\d\d\d-\d\d\d-\d\d\d\d', phonenumbertext)
+
+
+
+
+# quantifiers
+
+# +       occurs one or more times      
+# {3}     occurs exactly 3 times
+# {2,4}   Occurs 2 to 4 times
+# {3.}    Occurs 3 or more times        \w{3,}    anycharacters
+# *       Occurs zero or more times     A*B*C*    AAACC
+# ?       Once or none                  plurals?  plural
+
+phone = re.search(r'\d{3}-\d{3}-\d{4}', phonenumbertext)
+print(phone)
+
+phone_pattern = re.compile(r'(\d{3})-(\d{3})-(\d{4})')
+
+results = re.search(phone_pattern, phonenumbertext)
+print(results.group(1))
