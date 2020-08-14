70fe5eb5a143c07d82c16ef91d57ac9c22086d8b
Greg Stephen <GregAStephen@gmail.com>
2020-08-13 21:50:51 -0500

    finished collections module lesson

--- collections_module.py
+++ collections_module.py
@@ -18,13 +18,29 @@ c = Counter(letters)
 print(c.most_common(2))
 '''
 Common patterns for Counter() object
-sum(c.values()) total of all counts
-c.clear() reset all counts
-list(c) list unique elements
-set(c) convert to a set
-dict(c) convert to a reg dictionary
+sum(c.values())               total of all counts
+c.clear()                     reset all counts
+list(c)                       list unique elements
+set(c)                        convert to a set
+dict(c)                       convert to a reg dictionary
 Counter(dict(list_of_pairs))  convert from a list of (elem, cnt) pairs
-c.most_common()[:-n-1:-1] n least common elements
-c += Counter() remove zero and negative counts
+c.most_common()[:-n-1:-1] n   least common elements
+c += Counter()                remove zero and negative counts
 '''
 
+from collections import defaultdict
+# puts a default value if there is no key 
+d = defaultdict(lambda: 0)
+d['correct'] = 100
+print(d['correct'])
+print(d['Wrong key!'])
+
+
+mytuple = (10, 20, 30)
+
+
+from collections import namedtuple
+
+Dog = namedtuple('Dog', ['age', 'breed', 'name'])
+sammy = Dog(age=5, breed='Husky', name='Sam')
+print(sammy.breed)
