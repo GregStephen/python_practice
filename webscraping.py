6fdf20779f433ea61795bac82c439908cba6b59d
Greg Stephen <GregAStephen@gmail.com>
2020-08-14 21:47:20 -0500

    beginning of web scraping

---
+++ webscraping.py
@@ -0,0 +1,9 @@
+# Rules of Web Scraping
+# Always try to get permision before scraping
+# If you make too many scraping attempts or requests your ip address could be blocked
+# some sites automatically block scraping software
+
+# Limitations of Web Scraping
+# In genearl every website is unique which means every web scraping script is unique
+# A slight change or update to a website may completely break your web scraping script
+
